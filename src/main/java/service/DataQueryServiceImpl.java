fe2e4bf5f5543cf178ff8a0d08482bb15c47892a
Omri-Gal-Shenhav <shenhav.omri@gmail.com>
2021-05-30 00:14:02 +0300

    Added comments

--- src/main/java/service/DataQueryServiceImpl.java
+++ src/main/java/service/DataQueryServiceImpl.java
@@ -9,10 +9,17 @@ import service.Parser.QueryParser;
 
 import java.util.List;
 
+/**
+ * A simple service able to store and retrieve data.
+ */
 public class DataQueryServiceImpl implements DataQueryService {
 
-
-
+    /**
+     * Takes query as input and returns matching entries
+     * @param query valid query string
+     * @return matching entries
+     * @throws Exception NoDataFound or QueryParseException with relevant info
+     */
     public List<Item> query(String query) throws Exception {
         Operator q = QueryParser.stringToQuery(query);
         List<Item> items = Database.getInstance().performQuery(q);
@@ -22,6 +29,12 @@ public class DataQueryServiceImpl implements DataQueryService {
 
     }
 
+    /**
+     * Take entity and stores it. ID must remain unique.
+     * If record with given ID already exists, it should be overwritten
+     * @param Item entity to store
+     * @throws Exception QueryParseException with relevant info
+     */
     public void save(Item Item) throws Exception {
         Database.getInstance().insert(Item);
     }
