b0a92bc4ec5806a05482d96daedc58b40fd1d174
Omri-Gal-Shenhav <shenhav.omri@gmail.com>
2021-05-30 01:02:47 +0300

    Added tests for new operators

--- src/test/java/DataQueryServiceTest.java
+++ src/test/java/DataQueryServiceTest.java
@@ -1,5 +1,6 @@
 import api.DataQueryService;
 import api.Item;
+import api.NoDataFound;
 import api.QueryParseException;
 import org.junit.Test;
 import service.DataQueryServiceImpl;
@@ -98,5 +99,101 @@ public class DataQueryServiceTest {
     @Test(expected = QueryParseException.class)
     public void ReturnErrorWhenInvalidQueryPassed() throws Exception {
         dataQueryService.query("INVALID");
+    }
+
+    @Test
+    public void testUpdateOperator() throws Exception{
+        Item item1 = (Item) templateItem.clone();
+        item1.setId("id1");
+        item1.setViews(500);
+        dataQueryService.save(item1);
+        dataQueryService.query("UPDATE(views,1000,EQUAL(id,\"id1\"))");
+        assertEquals(true, dataQueryService.query("EQUAL(id,\"id1\")").get(0).getViews()==1000);
+    }
+
+    @Test(expected = NoDataFound.class)
+    public void testDeleteOperator() throws Exception{
+        Item item1 = (Item) templateItem.clone();
+        item1.setId("id1");
+        Item item2 = (Item) templateItem.clone();
+        item2.setId("id2");
+        dataQueryService.save(item1);
+        dataQueryService.query("DELETE(EQUAL(id,\"id1\"))");
+        assertEquals(true, dataQueryService.query("").containsAll(Arrays.asList(item2)));
+    }
+
+    @Test
+    public void testBetweenOperator() throws Exception{
+        Item item1 = (Item) templateItem.clone();
+        item1.setViews(1);
+        item1.setId("id1");
+        Item item2 = (Item) templateItem.clone();
+        item2.setId("id2");
+        item2.setViews(50);
+        Item item3 = (Item) templateItem.clone();
+        item3.setId("id3");
+        item3.setViews(20);
+
+        dataQueryService.save(item1);
+        dataQueryService.save(item2);
+        dataQueryService.save(item3);
+        assertEquals(true, dataQueryService.query("BETWEEN(views,5,100)").containsAll(Arrays.asList(item2,item3)));
+    }
+
+    @Test
+    public void testMinOperator() throws Exception{
+        Item item1 = (Item) templateItem.clone();
+        item1.setViews(1);
+        item1.setTitle("t");
+        item1.setId("id1");
+        Item item2 = (Item) templateItem.clone();
+        item2.setId("id2");
+        item2.setTitle("t");
+        item2.setViews(50);
+        Item item3 = (Item) templateItem.clone();
+        item3.setId("id3");
+        item3.setTitle("t");
+        item3.setViews(20);
+
+        dataQueryService.save(item1);
+        dataQueryService.save(item2);
+        dataQueryService.save(item3);
+        assertEquals(true, dataQueryService.query("MIN(views,EQUAL(title,\"t\"))").containsAll(Arrays.asList(item1)));
+    }
+
+    @Test
+    public void testMaxOperator() throws Exception{
+        Item item1 = (Item) templateItem.clone();
+        item1.setId("id1");
+        item1.setTitle("t");
+        item1.setViews(1);
+        Item item2 = (Item) templateItem.clone();
+        item2.setId("id2");
+        item2.setTitle("t");
+        item2.setViews(50);
+        Item item3 = (Item) templateItem.clone();
+        item3.setId("id3");
+        item3.setTitle("t");
+        item3.setViews(20);
+
+        dataQueryService.save(item1);
+        dataQueryService.save(item2);
+        dataQueryService.save(item3);
+        assertEquals(true, dataQueryService.query("MAX(views,EQUAL(title,\"t\"))").containsAll(Arrays.asList(item2)));
+    }
+
+    @Test
+    public void testAllOperator() throws Exception{
+        Item item1 = (Item) templateItem.clone();
+        item1.setId("id1");
+        Item item2 = (Item) templateItem.clone();
+        item2.setId("id2");
+        Item item3 = (Item) templateItem.clone();
+        item3.setId("id3");
+
+        dataQueryService.save(item1);
+        dataQueryService.save(item2);
+        dataQueryService.save(item3);
+        assertEquals(true, dataQueryService.query("ALL()").containsAll(Arrays.asList(item1, item2,item3)));
     }
 }
