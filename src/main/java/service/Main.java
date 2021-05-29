76310450e6751113c7a364d6940514899c88377e
Omri-Gal-Shenhav <shenhav.omri@gmail.com>
2021-05-30 00:13:45 +0300

    Fixed typo

--- src/main/java/service/Main.java
+++ src/main/java/service/Main.java
@@ -31,10 +31,7 @@ public class Main {
             System.out.println("tip: type 'help' for query options or 'exit' to abort");
             System.out.print("Enter valid query: ");
             String query = scan.nextLine();
-            if (query.equalsIgnoreCase("help")) {
-                printHelp();
-                scan.nextLine();
-            }
+            if (query.equalsIgnoreCase("help")) printHelp();
             else if(query.equalsIgnoreCase("exit")) keepLoop = false;
             else {
                 try {
@@ -57,19 +54,21 @@ public class Main {
 
     private static void printHelp(){
         System.out.println(
-                    "| EQUAL(property,value)  Filters only values which have matching property value.| EQUAL(id,\\\"first-post\\\")  EQUAL(views,100)   | \n" +
+                    "| EQUAL(property,value)  Filters only values which have matching property value.| EQUAL(id,\"s1\")| \n" +
                     "| GREATER_THAN(property,value)  Filters only values for which property is greater than the given value. Valid only for number values.| GREATER_THAN(views,100) | \n" +
                     "| LESS_THAN(property,value)  Filters only values for which property is less than the given value. Valid only for number values.| LESS_THAN(views,100) | \n" +
-                    "| AND(a,b)  Filters only values for which both a and b are true.| AND(EQUAL(id,\\\"first-post\\\"),EQUAL(views,100)) | \n" +
-                    "| OR(a,b)  Filters only values for which either a or b is true (or both).| OR(EQUAL(id,\\\"first-post\\\"),EQUAL(id,\\\"second-post\\\")) | \n" +
-                    "| NOT(a)  Filters only values for which a is false.| NOT(EQUAL(id,\\\"first-post\\\")) | \n" +
+                    "| AND(a,b)  Filters only values for which both a and b are true.| AND(EQUAL(id,\"s1\"),EQUAL(views,100)) | \n" +
+                    "| OR(a,b)  Filters only values for which either a or b is true (or both).| OR(EQUAL(id,\"s1\"),EQUAL(id,\"s2\")) | \n" +
+                    "| NOT(a)  Filters only values for which a is false.| NOT(EQUAL(id,\"s1\")) | \n" +
                     "| BETWEEN(property,value,value)   Filters only values for which property is between the given value. Valid only for number values.| BETWEEN(views,20,100) | \n" +
-                    "| UPDATE(property,value,a)   Update property where a is true| UPDATE(EQUAL(id,\\\"first-post\\\"),views,500) | \n" +
-                    "| DELETE(a)   Delete values where a is true| DELETE(EQUAL(id,\\\"first-post\\\")) | \n" +
-                    "| MAX(property,a)   Filters only value for which property is the maximum, valid only for number property and where a is true| MAX(views,EQUAL(title,\\\"My First Post\\\")) | \n" +
+                    "| UPDATE(property,value,a)   Update property where a is true| UPDATE(views,500,EQUAL(id,\"s1\")) | \n" +
+                    "| DELETE(a)   Delete values where a is true| DELETE(EQUAL(id,\"s1\")) | \n" +
+                    "| MAX(property,a)   Filters only value for which property is the maximum, valid only for number property and where a is true| MAX(views,EQUAL(title,\"My First Post\")) | \n" +
                     "| MIN(property,a)   Filters only value for which property is the minimum, valid only for number property and where a is true | MIN(views,GREATER_THAN(views,20)) | \n" +
                     "| ALL()   Return all values | ALL() |");
         System.out.println("Press enter to continue");
+        Scanner scan = new Scanner(System.in);
+        scan.nextLine();
     }
 
 }
