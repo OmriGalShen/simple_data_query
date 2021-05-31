ba049e806f447959ca767fbc3c2934c9b536eab5
Omri-Gal-Shenhav <shenhav.omri@gmail.com>
2021-05-31 13:45:49 +0300

    Added \n to prints

--- src/main/java/service/Main.java
+++ src/main/java/service/Main.java
@@ -53,18 +53,18 @@ public class Main {
 
     private static void printHelp(){
         System.out.println(
-                    "EQUAL(property,value) Filters only values which have matching property value. e.g: EQUAL(id,\"s1\") \n" +
-                    "GREATER_THAN(property,value) Filters only values for which property is greater than the given value. Valid only for number values. e.g: GREATER_THAN(views,100) \n" +
-                    "LESS_THAN(property,value)  Filters only values for which property is less than the given value. Valid only for number values. e.g:LESS_THAN(views,100) \n" +
-                    "AND(a,b)  Filters only values for which both a and b are true. e.g: AND(EQUAL(id,\"s1\"),EQUAL(views,100))\n" +
-                    "OR(a,b)  Filters only values for which either a or b is true (or both). e.g: OR(EQUAL(id,\"s1\"),EQUAL(id,\"s2\"))\n" +
-                    "NOT(a)  Filters only values for which a is false. e.g: NOT(EQUAL(id,\"s1\"))\n" +
-                    "BETWEEN(property,value,value) Filters only values for which property is between the given value. Valid only for number values. e.g: BETWEEN(views,20,100)\n" +
-                    "UPDATE(property,value,a) Update property where a is true. e.g: UPDATE(views,500,EQUAL(id,\"s1\"))\n" +
-                    "DELETE(a) Delete values where a is true. e.g: DELETE(EQUAL(id,\"s1\"))\n" +
-                    "MAX(property,a) Filters only value for which property is the maximum, valid only for number property and where a is true e.g:MAX(views,EQUAL(title,\"My First Post\"))\n" +
-                    "MIN(property,a) Filters only value for which property is the minimum, valid only for number property and where a is true e.g:MIN(views,GREATER_THAN(views,20))\n" +
-                    "ALL() Return all values. e.g:ALL()\n");
+                    "EQUAL(property,value) Filters only values which have matching property value.\n e.g: EQUAL(id,\"s1\") \n" +
+                    "GREATER_THAN(property,value) Filters only values for which property is greater than the given value. Valid only for number values.\n e.g: GREATER_THAN(views,100) \n" +
+                    "LESS_THAN(property,value)  Filters only values for which property is less than the given value. Valid only for number values.\n e.g:LESS_THAN(views,100) \n" +
+                    "AND(a,b)  Filters only values for which both a and b are true.\n e.g: AND(EQUAL(id,\"s1\"),EQUAL(views,100))\n" +
+                    "OR(a,b)  Filters only values for which either a or b is true (or both).\n e.g: OR(EQUAL(id,\"s1\"),EQUAL(id,\"s2\"))\n" +
+                    "NOT(a)  Filters only values for which a is false.\n e.g: NOT(EQUAL(id,\"s1\"))\n" +
+                    "BETWEEN(property,value,value) Filters only values for which property is between the given value. Valid only for number values.\n e.g: BETWEEN(views,20,100)\n" +
+                    "UPDATE(property,value,a) Update property where a is true.\n e.g: UPDATE(views,500,EQUAL(id,\"s1\"))\n" +
+                    "DELETE(a) Delete values where a is true.\n e.g: DELETE(EQUAL(id,\"s1\"))\n" +
+                    "MAX(property,a) Filters only value for which property is the maximum, valid only for number property and where a is true.\n e.g:MAX(views,EQUAL(title,\"My First Post\"))\n" +
+                    "MIN(property,a) Filters only value for which property is the minimum, valid only for number property and where a is true.\n e.g:MIN(views,GREATER_THAN(views,20))\n" +
+                    "ALL() Return all values.\n e.g:ALL()\n");
         System.out.print("Press enter to continue");
         Scanner scan = new Scanner(System.in);
         scan.nextLine();
